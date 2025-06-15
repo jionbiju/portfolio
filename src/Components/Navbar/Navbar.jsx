e2ef280bb33d4ada0defd7fd5ca4c2d1887abbf9
jionbiju <jionbiju@gmail.com>
2025-06-15 18:33:06 +0530

    navbar created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1 className='title'>JION<span >.</span></h1>
+      <div className="nav-menu-box">
+        <ul className="nav-menu">
+        <li>Home</li>
+        <li>About-me</li>
+        <li>Projects</li>
+        <li>Services</li>
+      </ul>
+      </div>
+      <div className="contact-btn">Contact Me</div>
+    </div>
+  )
+}
+
+export default Navbar

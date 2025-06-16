0820f4074fc547df28afd193eab8af647fe2cb15
jionbiju <jionbiju@gmail.com>
2025-06-16 12:48:16 +0530

    skills component created

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Skills.css'
+import { skills } from '../../assets/data/constants' 
+
+const Skills = () => {
+  return (
+    <div className='skills'>
+      <h2>Skills</h2>
+      <p>Here are some of my skills that I've been working on over the past 2 years.</p>
+      <div className="skills-container">
+        {skills.map((skill, index) => {
+            return (
+                <div className="skill-card" key={index}>
+                    <h3>{skill.title}</h3>
+                    <div className="skill-list">
+                        {skill.skills.map((item,idx) => {
+                            return (
+                                <div className="skill-item" key={idx}>
+                                    <img src={item.image} alt="" />
+                                    {item.name}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Skills
---
+++ src/assets/data/constants.js
@@ -0,0 +1,82 @@
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+            {
+                name: "Bootstrap",
+                image:
+                    "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+            {
+                name: "React JS",
+                image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+        ]
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Node Js",
+                image: "https://nodejs.org/static/images/logo.svg",
+            },
+            {
+                name: "Express Js",
+                image:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWVmXTRybjWV4i9IV85_D7GtvdrJtzYHe8kn5KnJudvtK-OYhpqwTP-2aDiWyKrZvkEtE&usqp=CAU",
+            },
+            {
+                name: "MongoDB",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+            },
+            {
+                name: "Firebase",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+        ]
+    },
+    {
+        title:"Others",
+        skills:[
+             {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGp2RKrzqJF2Cu3dxnk2RGK_F7SSloJG-Wyw&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://w7.pngwing.com/pngs/80/196/png-transparent-netlify-hd-logo-thumbnail.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-postman-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-brand-vol-5-pack-logos-icons-2945092.png",
+      },
+        ]
+    }
+]

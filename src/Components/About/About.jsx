be6af502b91b2283b0c112d8483df6c9cfdfecfe
jionbiju <jionbiju@gmail.com>
2025-06-20 10:57:33 +0530

    About section updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,21 +12,47 @@ const About = () => {
 
         <div className="about-text">
           <p>
-            I’m a 3rd-year Computer Science Engineering student at Vimal Jyothi Engineering College, Kerala. 
-            My passion for technology has led me to work on several college-level projects, helping me gain 
-            hands-on experience in software development and improve my problem-solving skills.
+            I’m a 3rd-year Computer Science Engineering student at Vimal Jyothi
+            Engineering College, Kerala. My passion for technology has led me to
+            work on several college-level projects, helping me gain hands-on
+            experience in software development and improve my problem-solving
+            skills.
           </p>
 
           <p>
-            I’ve built responsive web applications and explored various technologies — with a strong focus 
-            on frontend development using HTML, CSS, JavaScript, and React. I love creating clean, user-friendly 
-            interfaces and constantly strive to grow my skill set.
+            I love creating clean, user-friendly interfaces and constantly
+            strive to grow my skill set.I aim to become a well-rounded
+            full-stack developer and am excited about contributing to real-world
+            tech projects
           </p>
 
-          <p>
-            Outside of academics, I enjoy exploring new tools, libraries, and frameworks. I aim to become a 
-            well-rounded full-stack developer and am excited about contributing to real-world tech projects.
-          </p>
+          <div className="about-info-grid">
+            <div>
+              <strong>DOB:</strong> 17 June 2004
+            </div>
+            <div>
+              <strong>City:</strong> Kannur, Kerala
+            </div>
+            <div>
+              <strong>Freelance:</strong> Available
+            </div>
+            <div>
+              <strong>GitHub:</strong>{" "}
+              <a
+                href="https://github.com/jionbiju"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github.com/jionbiju
+              </a>
+            </div>
+            <div>
+              <strong>Phone: </strong> 9778368880
+            </div>
+            <div>
+              <strong>Email:</strong> jionbiju@gmail.com
+            </div>
+          </div>
         </div>
       </div>
     </div>
